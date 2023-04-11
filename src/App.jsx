a6846a4803381b8af4034a342a0fb0d4a02147e0
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-04-11 11:40:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import '/app.css'
+import fondo from '/images/verano.jpg'
+//style={{backgroundImage:`url(${fondo})`}}
+
+function App() {
+
+  return (
+    <div className="App " >
+      <section className='appClimate'>
+
+      </section>
+    </div>
+  )
+}
+
+export default App
