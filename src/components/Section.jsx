c7b8497defe1f7cebe189ca8190e0a35389d2cd8
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-04-16 22:03:31 -0500

    Changes: performance mejorado

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,27 @@
 import '../../public/styles.css'
+import { useEffect, useState } from 'react'
+import getCountries from '../services/getCountries'
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+
 const Section = () => {
+  const [countries, setContries] = useState([])
+  useEffect(() => {
+    (async () => {
+      setContries(await getCountries())
+    })()
+  }, [])
+  console.log(countries)
+
   return (
     <section className='appClimate'>
-      <p>Hola</p>
+      <Autocomplete
+        disablePortal
+        id='combo-box-demo'
+        options={countries.map(country => country.name.official)}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label='Escoge un Pais' />}
+      />
     </section>
   )
 }
