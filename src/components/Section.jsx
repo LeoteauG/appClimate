47b9b5ff82d704873e1595ed6ba649d12ce765c2
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-04-17 20:52:36 -0500

    Change: sección de paises creado

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,6 +4,7 @@ import getCountries from '../services/getCountries'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 
+// import getCities from '../services/getCities'
 const Section = () => {
   const [countries, setContries] = useState([])
   useEffect(() => {
@@ -11,13 +12,17 @@ const Section = () => {
       setContries(await getCountries())
     })()
   }, [])
-  console.log(countries)
-
+  const countryHandler = (e) => {
+    const country = e.currentTarget.value
+    console.log(country)
+  }
+  // console.log(city)
   return (
     <section className='appClimate'>
       <Autocomplete
         disablePortal
         id='combo-box-demo'
+        onChange={countryHandler}
         options={countries.map(country => country.name.official)}
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label='Escoge un Pais' />}
