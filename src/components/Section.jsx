42d60f2312313fb10b11a2daf70fb0ad82c19a8d
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-04-18 09:02:20 -0500

    Change: api de ciudades configurado

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,27 +3,40 @@ import { useEffect, useState } from 'react'
 import getCountries from '../services/getCountries'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
+import getCities from '../services/getCities'
 
 // import getCities from '../services/getCities'
 const Section = () => {
   const [countries, setContries] = useState([])
+  const [city, setCity] = useState(null)
+  const [ciudad, setCiudad] = useState([])
+
   useEffect(() => {
     (async () => {
       setContries(await getCountries())
+      setCiudad(await getCities(city))
     })()
-  }, [])
-  const countryHandler = (e) => {
-    const country = e.currentTarget.value
-    console.log(country)
+  }, [city])
+
+  const countryHandler = (event, valor) => {
+    const ciudad = countries.find(country => {
+      if (country.name.common === valor) {
+        return country.cca2
+      }
+      return null
+    }
+    )
+    setCity(ciudad)
   }
-  // console.log(city)
+
+  console.log(ciudad)
   return (
     <section className='appClimate'>
       <Autocomplete
         disablePortal
         id='combo-box-demo'
         onChange={countryHandler}
-        options={countries.map(country => country.name.official)}
+        options={countries.map(country => country.name.common)}
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label='Escoge un Pais' />}
       />
