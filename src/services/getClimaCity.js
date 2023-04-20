2d95b1fddabcff78580a0fa2fed9681a1881c599
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-04-20 00:05:35 -0500

    Change: se arreglo algunos detalles

--- src/services/getClimaCity.js
+++ src/services/getClimaCity.js
@@ -1,16 +1,16 @@
 import { ajax } from '../tools/ajax'
 
-const getClimaCity = (nameCity) => {
+const getClimaCity = async nameCity => {
   const options = {
     method: 'GET',
     url: 'https://api.openweathermap.org/data/2.5/weather',
     params: {
-      q: nameCity,
+      q: `${nameCity}`,
       appid: '083713cba91b0ba375f64d88f67e39e0',
       units: 'metric'
     }
-  }// https://api.openweathermap.org/data/2.5/weather?&id=&appid=083713cba91b0ba375f64d88f67e39e0
-  ajax(options)
+  }
+  return await ajax(options)
 }
 
 export default getClimaCity
